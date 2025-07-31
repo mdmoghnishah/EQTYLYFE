d3e1fb28bd23a798d5451655fcb474a7829c2264
mdmoghnishah <mdmoghnishah@gmail.com>
2025-07-31 10:08:35 +0530

    resolved logo

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaLinkedinIn, FaSquareFacebook, FaXTwitter } from 'react-icons/fa6';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 
 const FooterSection = () => {
   return (
